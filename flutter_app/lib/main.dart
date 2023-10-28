873ec3cf78f54b85b3589ac8f8cefc09e7cedcdb
HugoBianquini <hugobianqui@hotmail.com>
2023-10-28 16:31:21 -0300

    feat: configure initial route

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -22,7 +22,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LogInScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LogInScreen(),
+      },
     );
   }
 }
