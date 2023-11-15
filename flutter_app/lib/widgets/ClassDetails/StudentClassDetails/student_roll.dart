3afb0326e8e3a405ad6e94e1ee7e1787a070e66e
Adriano Souza <adrianosg@id.uff.br>
2023-11-15 17:42:34 -0300

    fixing issue 82

--- flutter_app/lib/widgets/ClassDetails/StudentClassDetails/student_roll.dart
+++ flutter_app/lib/widgets/ClassDetails/StudentClassDetails/student_roll.dart
@@ -45,19 +45,50 @@ Widget studentRoll(StudentRollData data) {
 
 Widget? getTrailing(bool isPresent) {
   if (!isPresent) {
+    bool fileSent = false;
+    return getOption(fileSent);     
+  }
+  
+  return Icon(Icons.check_circle_outline, color: Colors.green[800]);
+}
+
+Widget? getOption(bool fileSent) {
+  if (fileSent) {
     return ElevatedButton(
+      onPressed: () async {
+        //fazer a logica para puxar e visualizar o arquivo do BD
+      },
+      style: ElevatedButton.styleFrom(minimumSize: Size(115, 30)),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(vertical: 6),
+        child: Text(
+          'Visualizar\nAtestado',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+        ),
+      )
+    );   
+  }
+
+  return ElevatedButton(
         style: ElevatedButton.styleFrom(minimumSize: Size(115, 30)),
         onPressed: () async {
-          FilePickerResult? result = await FilePicker.platform.pickFiles();
+          FilePickerResult? result = await FilePicker.platform.pickFiles(
+            type: FileType.custom,
+            allowedExtensions: ['pdf','png', 'jpg', 'jpeg'],
+          );
 
           if(result != null) {
+            fileSent = true;
+            //fazer a logica para enviar o arquivo para o BD
+
             PlatformFile file = result.files.first;
             print(file.name);
             print(file.bytes);
             print(file.size);
             print(file.extension);
           } else {
-            // User canceled the picker
+            //Caso o usuario feche a aba de selecionar arquivos
           }
         },
         child: const Padding(
@@ -67,8 +98,6 @@ Widget? getTrailing(bool isPresent) {
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
           ),
-        ));
-  }
-
-  return Icon(Icons.check_circle_outline, color: Colors.green[800]);
+        )
+      );
 }
