a64628023d1c596c7cd251d277633d3ae02e4628
HugoBianquini <hugobianqui@hotmail.com>
2023-11-18 18:56:08 -0300

    create shared circular loading

---
+++ flutter_app/lib/widgets/shared/loading.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget circularLoading() {
+  return Center(
+      child: CircularProgressIndicator(
+    color: Colors.white,
+    backgroundColor: Colors.purple,
+  ));
+}
