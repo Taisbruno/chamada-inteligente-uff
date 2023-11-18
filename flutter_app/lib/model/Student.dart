f137b2ee2fb19b2daf910def10f68fa8083f82fa
HugoBianquini <hugobianqui@hotmail.com>
2023-11-18 18:53:17 -0300

    adjust student model to receive stats from backend

--- flutter_app/lib/model/Student.dart
+++ flutter_app/lib/model/Student.dart
@@ -1,16 +1,21 @@
 class Student {
   final String registration;
   final String name;
+  final double frequency;
+  final bool failed;
 
   const Student({
     required this.registration,
     required this.name,
+    this.frequency = 0,
+    this.failed = false,
   });
 
   factory Student.fromJson(Map<String, dynamic> json) {
     return Student(
-      registration: json['registration'],
-      name: json['name'],
-    );
+        registration: json['registration'],
+        name: json['name'],
+        frequency: json['frequency'],
+        failed: json['failed']);
   }
 }
