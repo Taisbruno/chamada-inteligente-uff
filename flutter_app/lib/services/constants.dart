2276e078ccc389aded42f9fac38f7e77873a7ada
HugoBianquini <hugobianqui@hotmail.com>
2023-11-21 21:16:57 -0300

    point requests to dev environment

--- flutter_app/lib/services/constants.dart
+++ flutter_app/lib/services/constants.dart
@@ -1,8 +1,8 @@
 // LOCAL
 
-const String baseApiUrl = 'http://localhost:8443';
-const String websocketServerUrl = 'ws://localhost:8443/websocket-endpoint';
+// const String baseApiUrl = 'http://localhost:8443';
+// const String websocketServerUrl = 'ws://localhost:8443/websocket-endpoint';
 
 // DEV
-// const String baseApiUrl = 'http://18.117.162.226:8443';
-// const String websocketServerUrl = 'ws://18.117.162.226:8443/websocket-endpoint';
+const String baseApiUrl = 'http://18.117.162.226:8443';
+const String websocketServerUrl = 'ws://18.117.162.226:8443/websocket-endpoint';
